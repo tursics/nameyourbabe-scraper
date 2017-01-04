fbb079a21162154be587e5f4e3afcaf489f15b8a
Thomas Tursics <thomas@tursics.de>
2017-01-04 17:31:39 +0100

    download and repair datasets

--- build.js
+++ build.js
@@ -13,6 +13,7 @@ function harvestMetadata(dataSources, harvestFinishCallback) {
 		dataData,
 		name,
 		uri,
+		cachePathName,
 		result,
 		nutsList = [],
 		counts,
@@ -30,7 +31,6 @@ function harvestMetadata(dataSources, harvestFinishCallback) {
 
 		if (processCount === dataSources.length) {
 			console.log('---------------- ----------- ----------------------------------------');
-
 			console.log(dataSources.length + ' meta data items collected');
 			console.log('');
 
@@ -55,7 +55,7 @@ function harvestMetadata(dataSources, harvestFinishCallback) {
 		if (a.name === b.name) {
 			return 0;
 		}
-		return (a.name > b.name) ? -1 : 1;
+		return (a.name < b.name) ? -1 : 1;
 	});
 
 	console.log('NUTS region      Update      Name');
@@ -94,8 +94,10 @@ function harvestMetadata(dataSources, harvestFinishCallback) {
 			nutsList.push(dataSources[i].nuts);
 			nutsList.forEach(nutsSortFunction);
 
-			file.download(uri, dataSources[i].nuts + '_' + String.fromCharCode(96 + counts[dataSources[i].nuts]), 'metadata', function (path, userData) {
-				file.loadJSON(path, function (json, dataSource) {
+			cachePathName = dataSources[i].nuts + '_' + String.fromCharCode(96 + counts[dataSources[i].nuts]);
+
+			file.download(uri, cachePathName, 'metadata', 'json', {i: i, cachePathName: cachePathName}, function (path, userData) {
+				file.loadJSON(path, userData, function (json, dataSource) {
 					if (json === null) {
 						result = {error: true, errorMsg: 'Could not download metadata'};
 					} else {
@@ -105,15 +107,16 @@ function harvestMetadata(dataSources, harvestFinishCallback) {
 						result = metadata.parse('', json);
 					}
 
-					result.sourceId = dataSource;
+					result.sourceId = dataSource.i;
+					result.cachePath = dataSource.cachePathName;
 
 					txt = '';
-					txt += dataSources[dataSource].nuts;
-					for (j = dataSources[dataSource].nuts.length; j < 17; ++j) {
+					txt += dataSources[dataSource.i].nuts;
+					for (j = dataSources[dataSource.i].nuts.length; j < 17; ++j) {
 						txt += ' ';
 					}
 					dataData = 'ERROR';
-					name = dataSources[dataSource].name;
+					name = dataSources[dataSource.i].name;
 
 					if (result.error) {
 						if ('-' === result.errorMsg) {
@@ -138,22 +141,85 @@ function harvestMetadata(dataSources, harvestFinishCallback) {
 					console.log(txt);
 
 					finishCallback(result.error ? null : result);
-				}, userData);
-			}, i);
+				});
+			});
 		}
 	}
 }
 
 //-----------------------------------------------------------------------
 
-function harvestDatasetBySource(harvest, source) {
+function harvestDatasetBySource(harvest, source, harvestFinishCallback) {
 	'use strict';
 
-	var txt, nuts, nutsVec, idx, name, url, path, num, j, file, zip, info, number,
-		fs = require('fs');
+	var txt, nuts, nutsVec, idx, name, url, ext, path, num, j, filePath, downloadName, zip, info, number,
+		fs = require('fs'),
+		file = require('./lib/file'),
+		processCount = 0;
 
-	console.log('Number Copy from                      To local path');
-	console.log('------ ------------------------------ ----------------------------------------');
+	function finishCallback(harvestResult) {
+		++processCount;
+
+		if (processCount >= harvestResult.vecURL.length) {
+			harvestFinishCallback(harvestResult);
+		}
+	}
+	function repairDataset(buffer, harvest, idx, txt) {
+		if (-1 !== buffer.toLowerCase().indexOf('<!doctype html')) {
+			// metadata in CKAN portal link to download page and not to the data
+			var path = harvest.vecDownload[idx];
+
+			file.loadTXT(path, {txt: txt, harvest: harvest, idx: idx}, function (txt, userData) {
+				var pos = txt.indexOf('class="download"'),
+					harvest = userData.harvest,
+					idx = userData.idx,
+					url = '',
+					downloadName,
+					ext;
+
+				if (-1 !== pos) {
+					pos = txt.indexOf('href="', pos) + 6;
+					if (-1 !== pos) {
+						url = txt.substr(pos, txt.indexOf('"', pos + 1) - pos);
+					}
+				}
+
+				harvest.vecDownload[idx] = '';
+
+				if (url.length === 0) {
+					userData.txt += '[failed to resolve file]';
+					console.log(userData.txt);
+					finishCallback(harvest);
+				} else {
+					downloadName = url.substr(url.lastIndexOf('/') + 1);
+					if (-1 !== downloadName.lastIndexOf('.')) {
+						downloadName = downloadName.substr(0, downloadName.lastIndexOf('.'));
+					}
+					ext = harvest.vecFormat[idx];
+
+					harvest.vecName[idx] = downloadName;
+
+					file.download(url, harvest.cachePath, downloadName, ext || 'json', {idx: idx, txt: userData.txt, harvest: harvest}, function (path, userData) {
+						var harvest = userData.harvest,
+							idx = userData.idx,
+							txt = userData.txt;
+
+						if (!fs.existsSync(path)) {
+							txt += '[failed to download file]';
+							console.log(txt);
+							finishCallback(harvest);
+						} else {
+							harvest.vecDownload[idx] = path;
+							repairDataset('', harvest, idx, txt);
+						}
+					});
+				}
+			});
+		} else {
+			console.log(txt);
+			finishCallback(harvest);
+		}
+	}
 
 	harvest.vecDownload = [];
 
@@ -171,6 +237,7 @@ function harvestDatasetBySource(harvest, source) {
 	for (idx = 0; idx < harvest.vecURL.length; ++idx) {
 		name = harvest.vecName[idx];
 		url = harvest.vecURL[idx];
+		ext = harvest.vecFormat[idx];
 
 //		if (0 === url.indexOf('/katalog/storage')) {
 //			url = 'http://data.gv.at' + url;
@@ -193,113 +260,142 @@ function harvestDatasetBySource(harvest, source) {
 
 		txt = '';
 
-		num = (idx + 1).toString();
+		txt += nuts;
+		for (j = nuts.length; j < 17; ++j) {
+			txt += ' ';
+		}
+
+		num = (idx + 1).toString() + '/' + (harvest.vecURL.length).toString();
 		txt += num;
 		for (j = num.length; j < 7; ++j) {
 			txt += ' ';
 		}
 
-		txt += name.substr(0, 30) + ' ';
-		for (j = name.length; j < 30; ++j) {
-			txt += ' ';
-		}
-
 		if ('' === path) {
-			txt += '[ignore path]';
+			txt += 'ignore path: ' + name.substr(0, 45) + ' ';
+			for (j = name.length; j < 45; ++j) {
+				txt += ' ';
+			}
+
+			console.log(txt);
+			finishCallback(harvest);
 		} else {
-			file = './harvest';
-			if (!fs.existsSync(file)) {
-				fs.mkdirSync(file);
-			}
-			file += '/' + nutsVec[idx].substr(0, 2);
-			if (!fs.existsSync(file)) {
-				fs.mkdirSync(file);
-			}
-			file += '/' + nutsVec[idx];
-			if (!fs.existsSync(file)) {
-				fs.mkdirSync(file);
+			txt += name.substr(0, 45) + ' ';
+			for (j = name.length; j < 45; ++j) {
+				txt += ' ';
 			}
 
-			path = 'harvest/' + nutsVec[idx].substr(0, 2) + '/' + nutsVec[idx] + '/' + path;
-			txt += path;
+			filePath = './harvest';
+//			if (!fs.existsSync(filePath)) {
+//				fs.mkdirSync(filePath);
+//			}
+			filePath += '/' + nutsVec[idx].substr(0, 2);
+//			if (!fs.existsSync(filePath)) {
+//				fs.mkdirSync(filePath);
+//			}
+			filePath += '/' + nutsVec[idx];
+//			if (!fs.existsSync(filePath)) {
+//				fs.mkdirSync(filePath);
+//			}
 
-			if (!copy(url, './' + path)) {
-				txt += '[failed to download file]';
-			} else {
-				harvest.vecDownload.push(path);
+			downloadName = path;
 
-				if ('.zip' === path.substr(-4)) {
-					zip = new ZipArchive();
-					zip.open('./' + path);
+//			path = 'harvest/' + nutsVec[idx].substr(0, 2) + '/' + nutsVec[idx] + '/' + path;
+//			txt += path;
 
-					for (num = 0; num < zip.numFiles; ++num) {
-						info = zip.statIndex(num);
-						number = (idx + 1) + '.' + (num + 1);
-						txt += '<br>' + number;
-						for (j = number.length; j < 37; ++j) {
-							txt += ' ';
+			file.download(url, harvest.cachePath, downloadName, ext || 'json', {idx: idx, txt: txt, harvest: harvest}, function (path, userData) {
+				var txt = userData.txt,
+					harvest = userData.harvest,
+					idx = userData.idx,
+					readStream;
+
+				if (!fs.existsSync(path)) {
+					txt += '[failed to download file]';
+					console.log(txt);
+					finishCallback(harvest);
+				} else {
+					harvest.vecDownload[idx] = path;
+
+					readStream = fs.createReadStream(path, {start: 0, end: 50});
+					readStream.on('open', function () {
+						this.myBuffer = '___';
+					});
+//					readStream.on('error', function(err) {
+//						res.end(err);
+//					});
+					readStream.on('data', function (buffer) {
+						this.myBuffer = buffer.toString();
+					});
+					readStream.on('end', function () {
+						repairDataset(this.myBuffer, harvest, idx, txt);
+					});
+
+/*					if ('.zip' === path.substr(-4)) {
+						zip = new ZipArchive();
+						zip.open('./' + path);
+
+						for (num = 0; num < zip.numFiles; ++num) {
+							info = zip.statIndex(num);
+							number = (idx + 1) + '.' + (num + 1);
+							txt += '<br>' + number;
+							for (j = number.length; j < 37; ++j) {
+								txt += ' ';
+							}
+
+							path = 'harvest/' + nutsVec[idx].substr(0, 2) + '/' + nutsVec[idx] + '/zip_' + info.name.substr(info.name.lastIndexOf('/') + 1);
+							txt += path;
+
+							url = 'zip://' + zip.filename + '#' + info.name;
+
+							if (!copy(url, './' + path)) {
+								txt += '[failed to unzip file ' + info.name + ']';
+							} else {
+								harvest.vecDownload.push(path);
+							}
 						}
-
-						path = 'harvest/' + nutsVec[idx].substr(0, 2) + '/' + nutsVec[idx] + '/zip_' + info.name.substr(info.name.lastIndexOf('/') + 1);
-						txt += path;
-
-						url = 'zip://' + zip.filename + '#' + info.name;
-
-						if (!copy(url, './' + path)) {
-							txt += '[failed to unzip file ' + info.name + ']';
-						} else {
-							harvest.vecDownload.push(path);
-						}
-					}
+					}*/
 				}
-			}
+			});
 		}
-		console.log(txt);
 	}
 
-	console.log('------ ------------------------------ ----------------------------------------');
-
-//	$HarvestMetadata->save();
+	if (harvest.vecURL.length === 0) {
+		finishCallback(harvest);
+	}
 }
 
 //-----------------------------------------------------------------------
 
-function harvestDatasets(dataSources, metadataResultList) {
+function harvestDatasets(dataSources, metadataResultList, harvestFinishCallback) {
 	'use strict';
 
-	var txt, i, harvest, source;
+	var txt, i, harvest, source,
+		downloadCount = 0,
+		datasetResultList = [];
 
-	console.log('Update source data');
-	console.log('==================');
-	console.log('');
+	function finishCallback(harvestResult) {
+		datasetResultList.push(harvestResult);
+		downloadCount += harvestResult.vecDownload.length;
+
+		if (datasetResultList.length === metadataResultList.length) {
+			console.log('---------------- ------ ---------------------------------------------');
+			console.log(downloadCount + ' data sets collected');
+			console.log('');
+
+			harvestFinishCallback(datasetResultList);
+		}
+	}
+
+	console.log('NUTS region      Number Source URL');
+	console.log('---------------- ------ ---------------------------------------------');
 
 	for (i = 0; i < metadataResultList.length; ++i) {
 		harvest = metadataResultList[i];
 		source = dataSources[harvest.sourceId];
 
-		if ('DEA22' === source.nuts) {
-//			console.log('Name:    ' + nutsGetName( source.nuts)['en-US']);
-			console.log('NUTS:    ' + source.nuts);
-			if (typeof source.nutsVec === 'undefined') {
-				txt = '         ';
-/*				foreach( array_unique( source.nutsVec) as nuts) {
-					txt += nuts + ' ';
-				}*/
-				console.log(txt);
-			}
-			console.log('Comment: ' + source.name);
-			txt = 'Update:  available since ' + harvest.modDays;
-			if (1 === harvest.modDays) {
-				txt += ' day';
-			} else {
-				txt += ' days';
-			}
-			console.log(txt);
-			console.log();
-
-			harvestDatasetBySource(harvest, source);
-			break;
-		}
+		harvestDatasetBySource(harvest, source, function (harvestResult) {
+			finishCallback(harvestResult);
+		});
 	}
 }
 
@@ -309,9 +405,11 @@ function start() {
 	'use strict';
 
 	var file = require('./lib/file');
-	file.loadJSON('./data/sources.json', function (dataSources) {
+	file.loadJSON('./data/sources.json', null, function (dataSources) {
 		harvestMetadata(dataSources, function (metadataResultList) {
-			harvestDatasets(dataSources, metadataResultList);
+			harvestDatasets(dataSources, metadataResultList, function (datasetResultList) {
+				
+			});
 		});
 	});
 }
